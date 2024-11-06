34e221742bdd201e47c2a0276ddd7f8bbbdcfd5b
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-11-06 18:00:13 +0100

    manufacturer url fix

--- src/upload/catalog/controller/extension/module/ps_live_search.php
+++ src/upload/catalog/controller/extension/module/ps_live_search.php
@@ -119,7 +119,7 @@ class ControllerExtensionModulePsLiveSearch extends Controller
                 }
 
                 $json['manufacturers']['data'][] = [
-                    'href' => $this->url->link('product/manufacturer.info', 'manufacturer_id=' . $manufacturerResult['manufacturer_id']),
+                    'href' => $this->url->link('product/manufacturer/info', 'manufacturer_id=' . $manufacturerResult['manufacturer_id']),
                     'name' => $manufacturerResult['name'],
                     'thumb' => $thumb,
                     'thumb_width' => $this->config->get('module_ps_live_search_manufacturer_image_width'),

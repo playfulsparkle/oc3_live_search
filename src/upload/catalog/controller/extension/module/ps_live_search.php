00a58b79ad6f54456b76c2e7551e5e325833cb82
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2025-05-07 13:34:20 +0200

    bumped to version 1.0.2, advanced JSON data structure validation

--- src/upload/catalog/controller/extension/module/ps_live_search.php
+++ src/upload/catalog/controller/extension/module/ps_live_search.php
@@ -10,7 +10,6 @@ class ControllerExtensionModulePsLiveSearch extends Controller
         }
 
         $json = array(
-            'query' => html_entity_decode($search, ENT_QUOTES, 'UTF-8'),
             'products' => array(
                 'status' => (bool) $this->config->get('module_ps_live_search_product_status'),
                 'show_price' => (bool) $this->config->get('module_ps_live_search_product_price'),
